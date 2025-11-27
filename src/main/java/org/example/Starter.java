655bfeaf7f31cdc9f32b54c9f85385456f5e93c6
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-11-27 12:28:02 +0530

    Initial commit

---
+++ src/main/java/org/example/Starter.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Starter extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/dashbord_form.fxml"))));
+        stage.show();
+    }
+}
