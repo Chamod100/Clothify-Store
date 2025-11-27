637c41b30cafdc42d87d81e2564eabd4924fcffd
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-11-27 22:24:11 +0530

    Implement dashboard form controller

--- src/main/java/org/example/controller/DashbordFormController.java
+++ src/main/java/org/example/controller/DashbordFormController.java
@@ -1,4 +1,69 @@
 package org.example.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
 public class DashbordFormController {
+
+    @FXML
+    private AnchorPane LodeFormContent;
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblTime;
+
+    @FXML
+    private Label lblUserAType;
+
+    @FXML
+    private Label lblUserEmail1;
+
+    @FXML
+    private Label lblUserName;
+
+
+    @FXML
+    void btnEmployeeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderDetailsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReportsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSalesReturnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupplierOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logOutBtnOnAction(ActionEvent event) {
+
+    }
+
 }
