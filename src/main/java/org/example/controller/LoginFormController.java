4c6b7ae3c5f92e8df6d78fb68bb4911ea4e302f7
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-12-03 23:23:20 +0530

    Implement login form UI and controller functionality

---
+++ src/main/java/org/example/controller/LoginFormController.java
@@ -0,0 +1,90 @@
+package org.example.controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import org.example.Model.Entity.User;
+import org.example.Utill.CrudUtill;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginFormController {
+
+    public JFXTextField txtUserName;
+    public JFXPasswordField txtPassword;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+
+        User user = getLogInUser();
+        if (user != null) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/dashbord_form.fxml"));
+                // fxmlLoader.setControllerFactory(controllerClass -> new DashBordFormController(user));
+                Parent root1 = (Parent) fxmlLoader.load();
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root1));
+                // Get the current window
+                Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                // Close the previous window
+                currentStage.close();
+                stage.setResizable(false);
+                stage.show();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            new Alert(Alert.AlertType.ERROR, "Your Password or UserName incorrect try again!").show();
+        }
+
+
+    }
+
+    public User getLogInUser() {
+        try {
+            ResultSet rst = CrudUtill.execute("SELECT * FROM user\n" +
+                    "WHERE UserName = ? AND password = ? AND (user_type = 'admin' OR user_type = 'user');", txtUserName.getText(), txtPassword.getText());
+            while (rst.next()) {
+                return new User(
+                        rst.getInt(1),
+                        rst.getString(2),
+                        rst.getString(3),
+                        rst.getString(4),
+                        rst.getString(5));
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    public void lblMouseClickOnAction(MouseEvent mouseEvent) {
+
+    }
+
+    public void lblRegisterMouseClickOnAction(MouseEvent mouseEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/user_registration_form.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            // Get the current window
+            Stage currentStage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+            // Close the previous window
+            currentStage.close();
+            stage.setResizable(false);
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
