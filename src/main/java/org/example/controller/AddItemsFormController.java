645cdb062a6c0d2599dd7008b3c5fea74f6ee422
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-12-07 23:38:39 +0530

    Add Add Items form and controller

---
+++ src/main/java/org/example/controller/AddItemsFormController.java
@@ -0,0 +1,28 @@
+package org.example.controller;
+
+import org.example.remository.DaoFactory;
+import org.example.remository.custom.ItemDao;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class AddItemsFormController {
+
+    public AnchorPane LodeFormContent;
+
+    ItemDao itemDao = DaoFactory.getDaoFactory().getDaoType(DaoFactory.DaoType.ITEM);
+    //-----------------------------------------------------------------------------------
+    public void backBtnOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = this.getClass().getResource("/view/dashbord_form.fxml");
+
+        assert resource != null;
+
+        Parent load = FXMLLoader.load(resource);
+        this.LodeFormContent.getChildren().clear();
+        this.LodeFormContent.getChildren().add(load);
+    }
+}
