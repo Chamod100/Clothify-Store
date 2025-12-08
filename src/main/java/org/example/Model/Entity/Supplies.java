04a6c1e0067166151a889c7f4d7a8f056905d387
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-12-08 22:45:25 +0530

    Add Supplies entity

---
+++ src/main/java/org/example/Model/Entity/Supplies.java
@@ -0,0 +1,21 @@
+package org.example.Model.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "supplies")
+public class Supplies {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String itemCode;
+    private String description;
+    private int qty;
+}
