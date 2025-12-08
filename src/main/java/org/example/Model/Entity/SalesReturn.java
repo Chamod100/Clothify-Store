93e6f23d3a02b7ef92dcff02a48e44197a94507d
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-12-08 22:50:03 +0530

    Add Sales Return form with FXML layout, controller, and entity classes

---
+++ src/main/java/org/example/Model/Entity/SalesReturn.java
@@ -0,0 +1,20 @@
+package org.example.Model.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "sales_return")
+public class SalesReturn {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String returnId;
+    private String orderId;
+    private double total;
+    private String date;
+}
---
+++ src/main/java/org/example/Model/Entity/SalesReturnDetails.java
@@ -0,0 +1,21 @@
+package org.example.Model.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "sales_return_details")
+public class SalesReturnDetails {
+    private String returnId;
+    private String itemCode;
+    private int qty;
+    private double discount;
+    private double unitPrice;
+    private double amount;
+}
