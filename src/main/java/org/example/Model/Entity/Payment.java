6b0e5121a665cfb03e5a714c1a3ef1a2425f38a9
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-12-09 23:42:41 +0530

    Add Payment entity with JPA annotations and Lombok support

---
+++ src/main/java/org/example/Model/Entity/Payment.java
@@ -0,0 +1,22 @@
+package org.example.Model.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "payment")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String paymentId;
+    private double cash;
+    private double balance;
+    private String date;
+    private boolean isPayByCash;
+    private String orderId;
+}
