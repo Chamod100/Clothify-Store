5e7d30599b7c2bb52a7152c92bcd6732ae4a4516
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-12-09 23:44:04 +0530

    Add Supplier and SupplierInvoice entities

---
+++ src/main/java/org/example/Model/Entity/Supplier.java
@@ -0,0 +1,22 @@
+package org.example.Model.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Setter
+@Getter
+@Entity
+@Table(name = "supplier")
+public class Supplier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer supplierId;
+    private String title;
+    private String supplierName;
+    private String contact;
+    private String company;
+    private String email;
+}
---
+++ src/main/java/org/example/Model/Entity/SupplierInvoice.java
@@ -0,0 +1,21 @@
+package org.example.Model.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@Entity
+@Table(name = "supplier_invoice")
+public class SupplierInvoice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String invoiceId;
+    private String supplierId;
+    private String itemCode;
+    private String date;
+    private int qty;
+}
